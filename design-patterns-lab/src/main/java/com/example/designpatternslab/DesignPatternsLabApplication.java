59349cc39fa9810396b1e612f5758dca049e78a3
AndreiBota <andrei.bota00@e-uvt.ro>
2025-10-09 09:38:06 +0300

    Implemented the Element abstract class

--- design-patterns-lab/src/main/java/com/example/designpatternslab/DesignPatternsLabApplication.java
+++ design-patterns-lab/src/main/java/com/example/designpatternslab/DesignPatternsLabApplication.java
@@ -1,6 +1,5 @@
 package com.example.designpatternslab;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -8,18 +7,30 @@ public class DesignPatternsLabApplication {
 
     public static void main(String[] args) {
 
-    Book book = new Book("Book1");
-    book.addAuthor(new Author("Author1"));
-    book.addAuthor(new Author("Author2"));
+    Book noapteBuna = new Book("Noapte buna, copii!");
+    Author rpGheo = new Author("Radu Pavel", "Gheo");
+    noapteBuna.addAuthor(rpGheo);
 
-    Chapter chapter1 = book.addChapter("Chapter1");
-    SubChapter subChapter1 = chapter1.addSubChapter("SubChapter1");
+    Section cap1 = new Section("Capitolul 1");
+    Section subcap11 = new Section("Subcapitolul 1.1");
+    Section subcap111 = new Section("Subcapitolul 1.1.1");
+    Section subcap1111 = new Section("Subcapitolul 1.1.1.1");
 
-    subChapter1.addParagraph(new Paragraph("Paragraph1"));
-    subChapter1.addImage(new Image("Image1"));
-    subChapter1.addTable(new Table("Table1"));
+    noapteBuna.add(new Paragraph("Multumesc celor care..."));
 
-    book.print();
+    noapteBuna.add(cap1);
+    cap1.add(new Paragraph("Moto capitol"));
+
+    cap1.add(subcap11);
+    subcap11.add(new Paragraph("Text from subchapter 1.1"));
+
+    subcap11.add(subcap111);
+    subcap111.add(new Paragraph("Text from subchapter 1.1.1"));
+
+    subcap111.add(subcap1111);
+    subcap1111.add(new Image("Image from subchapter 1.1.1.1"));
+
+    noapteBuna.print();
 
     }
 
---
+++ design-patterns-lab/src/main/java/com/example/designpatternslab/Element.java
@@ -0,0 +1,17 @@
+package com.example.designpatternslab;
+
+abstract class Element {
+    public abstract void print();
+
+    public void add(Element element){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void remove(Element element){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Element get(int index){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
